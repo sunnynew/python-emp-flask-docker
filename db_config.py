cbe7530a825b0257227b70f97c926a7bac019445
sunnynew <mail2connect@gmail.com>
2019-08-15 00:41:22 +0530

    first commit

--- db_config.py
+++ db_config.py
@@ -4,17 +4,17 @@ import os
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'db_emp'
+# We set Env variables during app deployment time. [Ansible or Kubernetes]
 
-#We are making /etc/hosts file entry on webservers for MySQL server through Ansible playbook. Please check app_playbook.yml for more details. Better to have a route53 inernal DNS configured.
-#app.config['MYSQL_DATABASE_HOST'] = 'mysql_server'
-#app.config['MYSQL_DATABASE_HOST'] = '10.31.33.208'
-
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
 
 #For localhost testing.
 #app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#app.config['MYSQL_DATABASE_USER'] = ''
+#app.config['MYSQL_DATABASE_PASSWORD'] = ''
+#app.config['MYSQL_DATABASE_DB'] = 'db_emp'
 
 mysql.init_app(app)
